c97990ac85180666fe6ae57a3744f9584911526e
Paul emmanuel LEVY <paulemmanuellevy@gmail.com>
2015-11-03 12:21:30 +0100

    Card added no design

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
     @spot = Spot.new
+    @allspot = Spot.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Spot.create(title: "Nice flat", description: "Sweet and smooth" , price: 14, address: "41 avenue des flouts" )
+Spot.create(title: "Nice flat 2", description: "Sweet and smooth 2" , price: 14, address: "42 avenue des flouts" )
+Spot.create(title: "Nice flat 3", description: "Sweet and smooth 3" , price: 14, address: "43 avenue des flouts" )
+Spot.create(title: "Nice flat 4", description: "Sweet and smooth 4" , price: 14, address: "44 avenue des flouts" )
