64c4546358bc7178cd48dca1566a2959174f2543
Paul emmanuel LEVY <paulemmanuellevy@gmail.com>
2015-11-04 16:54:29 +0100

    Final spot new page after comments and conflicts fixed

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,6 +2,13 @@ class Spot < ActiveRecord::Base
   alias_attribute :user, :owner
   belongs_to  :user
 
+#Il faudrait peut-etre soit rajouter un champs "visible" soit
+# mettre les champs correspondants à ces attributs
+# en obligatoire pour éviter qu'on affiche des spots
+# non finis - a voir si on fait un filtre au niveau de l'index
+# et de la show
+
+
   # validates :title, presence: true
   # validates :description, presence: true
   # validates :price, presence: true
