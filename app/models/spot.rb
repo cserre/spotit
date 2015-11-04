ad9812ef5713cd7484f767558eae2d5e2c859700
Paul emmanuel LEVY <paulemmanuellevy@gmail.com>
2015-11-04 13:25:57 +0100

    Basic new spot page and rediection to edit page

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,9 +2,9 @@ class Spot < ActiveRecord::Base
   alias_attribute :user, :owner
   belongs_to  :user
 
-  validates :title, presence: true
-  validates :description, presence: true
-  validates :price, presence: true
-  validates :address, presence: true
+  # validates :title, presence: true
+  # validates :description, presence: true
+  # validates :price, presence: true
+  # validates :address, presence: true
 
 end
