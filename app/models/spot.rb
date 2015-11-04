9f0ffa1872ea4458372e0e6b1d3cd76e560540f2
Charlotte Serre <c.serre41@gmail.com>
2015-11-04 21:26:17 +0100

    new spot ok, edit started, commit before migration

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,29 +2,18 @@ class Spot < ActiveRecord::Base
   alias_attribute :user, :owner
   belongs_to  :user
 
-#Il faudrait peut-etre soit rajouter un champs "visible" soit
-# mettre les champs correspondants à ces attributs
-# en obligatoire pour éviter qu'on affiche des spots
-# non finis - a voir si on fait un filtre au niveau de l'index
-# et de la show
-
-
   # validates :title, presence: true
   # validates :description, presence: true
   # validates :price, presence: true
-  # validates :address, presence: true
+  validates :city, presence: true
+  validates :area, presence: true
 
   paginates_per 20
 
   geocoded_by :address
-  after_validation :geocode, if: :address_changed?
+  after_validation :geocode, if: :street_changed? || :post_code_changed? || :city_changed?
 
   def address
     [street, post_code, city].compact.join(', ')
   end
-
-  def address_changed?
-    street_changed? || post_code_changed? || city_changed?
-  end
-
 end
